e3ecbdd461400811cd4f2f14d4afe97a0060720b
birkan <b.561birkan@gmail.com>
2022-07-14 18:23:45 +0300

    making responsive Navbar component

--- src/components/globalStyles/theme.js
+++ src/components/globalStyles/theme.js
@@ -1,5 +1,13 @@
 const theme = {
-  colors: {},
+  colors: {
+    navBgColor: "#e1f1dd",
+    mainColor: "#00adb5",
+    logoColor: "#393e46",
+    linkColor: "#02475e",
+  },
+  screens: {
+    lg: "900px",
+  },
 };
 
 export default theme;
