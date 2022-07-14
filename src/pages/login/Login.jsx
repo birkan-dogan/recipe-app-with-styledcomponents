243f0155cca0c8c45f55029a76a1d0d846e1d7d9
birkan <b.561birkan@gmail.com>
2022-07-14 17:27:15 +0300

    Login comp. completed and styled with Flex comp.

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,6 +1,38 @@
-import LoginStyle from "./Login.style";
+import LoginStyle, {
+  FormContainer,
+  Header,
+  LoginContainer,
+  StyledButton,
+  StyledForm,
+  StyledImg,
+  StyledInput,
+} from "./Login.style";
+import mealSvg from "../../assets/meal.svg";
+import { useNavigate } from "react-router-dom";
 const Login = () => {
-  return <div>Login</div>;
+  const userInfo = {
+    username: "birkan",
+  };
+
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    sessionStorage.setItem("user", JSON.stringify(userInfo));
+    navigate(-1);
+  };
+  return (
+    <LoginContainer>
+      <FormContainer>
+        <StyledImg src={mealSvg} />
+        <Header>{"<birkan/>"} recipe</Header>
+        <StyledForm onSubmit={handleSubmit}>
+          <StyledInput type="text" placeholder="username" required />
+          <StyledInput type="password" placeholder="password" required />
+          <StyledButton>Login</StyledButton>
+        </StyledForm>
+      </FormContainer>
+    </LoginContainer>
+  );
 };
 
 export default Login;
