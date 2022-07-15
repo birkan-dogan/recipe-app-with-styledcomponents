28811c0281ddd0ef7472096afcb38778bc8a8644
birkan <b.561birkan@gmail.com>
2022-07-15 09:46:38 +0300

    Header comp. is completed which is part of Home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,39 @@
 import HomeStyle from "./Home.style";
+import axios from "axios";
+import { useState } from "react";
+import Header from "../../components/header/Header";
 
 const Home = () => {
-  return <div>Home</div>;
+  const mealType = ["Breakfast", "Lunch", "Dinner", "Snack", "TeaTime"];
+  const [query, setQuery] = useState("egg");
+  const [selectedMeal, setSelectedMeal] = useState(mealType[0]);
+  const [recipes, setRecipes] = useState("");
+
+  const APP_ID = "05ffca8e";
+  const APP_KEY = "e8a3992fdde40b759ecdb5d9c39147a4";
+
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${selectedMeal}`;
+
+  const getData = async () => {
+    try {
+      const { data } = await axios.get(url);
+      // console.log(data.hits);
+      setRecipes(data.hits);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  console.log(recipes);
+  return (
+    <div>
+      <Header
+        setQuery={setQuery}
+        setSelectedMeal={setSelectedMeal}
+        mealType={mealType}
+        getData={getData}
+      />
+    </div>
+  );
 };
 
 export default Home;
