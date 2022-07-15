7a5e8c353aea559c156692167fd3e7c4bd05924d
birkan <b.561birkan@gmail.com>
2022-07-15 10:05:46 +0300

    conditional rendering to show data and avoid error

--- src/components/globalStyles/Global.styles.jsx
+++ src/components/globalStyles/Global.styles.jsx
@@ -9,5 +9,6 @@ export const GlobalStyles = createGlobalStyle`
     }
     body{
         font-size: 1.5rem;
+        background-color: ${({ theme }) => theme.colors.mainColor};
     }
 `;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,15 @@
-import HomeStyle from "./Home.style";
+import HomeStyle, { HeaderText, HomeImg, ImgDiv } from "./Home.style";
 import axios from "axios";
 import { useState } from "react";
 import Header from "../../components/header/Header";
+import Cards from "../../components/cards/Cards";
+import homeSvg from "../../assets/home.svg";
 
 const Home = () => {
   const mealType = ["Breakfast", "Lunch", "Dinner", "Snack", "TeaTime"];
-  const [query, setQuery] = useState("egg");
+  const [query, setQuery] = useState("");
   const [selectedMeal, setSelectedMeal] = useState(mealType[0]);
-  const [recipes, setRecipes] = useState("");
+  const [recipes, setRecipes] = useState(null);
 
   const APP_ID = "05ffca8e";
   const APP_KEY = "e8a3992fdde40b759ecdb5d9c39147a4";
@@ -15,15 +17,17 @@ const Home = () => {
   const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${selectedMeal}`;
 
   const getData = async () => {
-    try {
-      const { data } = await axios.get(url);
-      // console.log(data.hits);
-      setRecipes(data.hits);
-    } catch (error) {
-      console.log(error);
+    if (query) {
+      try {
+        const { data } = await axios.get(url);
+        // console.log(data.hits);
+        setRecipes(data.hits);
+      } catch (error) {
+        console.log(error);
+      }
     }
   };
-  console.log(recipes);
+  // console.log(recipes);
   return (
     <div>
       <Header
@@ -32,6 +36,15 @@ const Home = () => {
         mealType={mealType}
         getData={getData}
       />
+      {!recipes && (
+        <ImgDiv>
+          <HomeImg src={homeSvg} />
+        </ImgDiv>
+      )}
+      {recipes?.length === 0 && (
+        <HeaderText>The Food can not be found</HeaderText>
+      )}
+      {recipes?.length > 0 && <Cards recipes={recipes} />}
     </div>
   );
 };
