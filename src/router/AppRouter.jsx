eaf2448d513c982c9e64c9151561fc40b07be57b
birkan <b.561birkan@gmail.com>
2022-07-15 13:10:35 +0300

    Register component done

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -4,6 +4,7 @@ import About from "../pages/about/About";
 import Detail from "../pages/detail/Detail";
 import Home from "../pages/home/Home";
 import Login from "../pages/login/Login";
+import Register from "../pages/register/Register";
 import PrivateRouter from "./PrivateRouter";
 const AppRouter = () => {
   return (
@@ -12,6 +13,7 @@ const AppRouter = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
         <Route path="about" element={<PrivateRouter />}>
           <Route path="" element={<About />} />
         </Route>
